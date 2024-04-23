b99bf34f229e1afa0a4f67d00ede95226ee9b3dc
Edwar León <edwar.leon.m@gmail.com>
2024-04-22 23:22:53 -0500

    Fix middle number data

--- src/index.js
+++ src/index.js
@@ -2,30 +2,37 @@ import { printData } from "./utils/printData.js"
 
 export const compareNumbers = (n1, n2, n3) => {
     const numberArr = [n1, n2, n3]
-    const cloneArr1 =  structuredClone(numberArr)
-    const cloneArr2 = structuredClone(numberArr)
-    const sortlow = cloneArr1.sort((a,b) => b-a)
-    const sortHig = cloneArr2.sort((a,b) => a-b)
+    // const cloneArr1 =  structuredClone(numberArr)
+    // const cloneArr2 = structuredClone(numberArr)
+    // const sortlow = cloneArr1.sort((a,b) => b-a)
+    // const sortHig = cloneArr2.sort((a,b) => a-b)
     
     let numeroAlto = 0
     let numeroBajo = numberArr[0]
     let numeroMedio
     let valorRepetido = 'No hay números repetidos'
 
-   for(let i = 0; i < numberArr.length; i++) {
-    if(numberArr[i] > numeroAlto) numeroAlto = numberArr[i]
-    if(numberArr[i] < numeroBajo) numeroBajo = numberArr[i]
-    for(let j = 0; j < numberArr.length; j++) {
-        if(numberArr[i] === numberArr[j] && i !== j) valorRepetido = `El número ${numberArr[i]} está repetido`
-    }
+    for(let i = 0; i < numberArr.length; i++) {
+        if(numberArr[i] > numeroAlto) numeroAlto = numberArr[i]
+        if(numberArr[i] < numeroBajo) numeroBajo = numberArr[i]
+
+        for(let j = 0; j < numberArr.length; j++) {
+            if(numberArr[i] === numberArr[j] && i !== j) {
+                valorRepetido = `El número ${numberArr[i]} está repetido`
+                numeroMedio = numberArr[i]
+            }
+        }
    }
 
-   for(let i = 0; i < numberArr.length; i++) {
-    if((numberArr[i] < numeroAlto
-        && numberArr[i] > numeroBajo))
-        numeroMedio = numberArr[i]
+    for(let i = 0; i < numberArr.length; i++) {
+        if((numberArr[i] < numeroAlto
+            && numberArr[i] > numeroBajo))
+            numeroMedio = numberArr[i]
    }
    
+    const sortHig = [numeroBajo, numeroMedio, numeroAlto]
+    const sortlow = [numeroAlto, numeroMedio, numeroBajo]
+
     const data = {
         numeroAlto,
         numeroBajo,
